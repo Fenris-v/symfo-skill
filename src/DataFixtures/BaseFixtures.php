8079cb31113fd71ebbbce79e5c54e9463c041543
fenris <Fenris-v>
2021-07-29 23:47:34 +0300

    fixtures

---
+++ src/DataFixtures/BaseFixtures.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+abstract class BaseFixtures extends Fixture
+{
+    protected Generator $faker;
+    protected ObjectManager $manager;
+
+    public function load(ObjectManager $manager)
+    {
+        $this->faker = Factory::create();
+        $this->manager = $manager;
+
+        $this->loadData($manager);
+    }
+
+    abstract function loadData(ObjectManager $manager);
+
+    protected function create(string $className, callable $factory)
+    {
+        $entity = new $className();
+        $factory($entity);
+
+        $this->manager->persist($entity);
+
+        return $entity;
+    }
+
+    protected function createMany(
+        string $className,
+        int $count,
+        callable $factory
+    ) {
+        for ($i = 0; $i < $count; $i++) {
+            $this->create($className, $factory);
+        }
+
+        $this->manager->flush();
+    }
+}
